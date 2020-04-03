136c472db91645340bc438524a57e034aa7d60bb
Narendra-gali <narruedu@gmail.com>
2020-04-03 12:23:47 +0000

    FirstCommit springboot

---
+++ src/main/java/com/mindtree/DemoCheck.java
@@ -0,0 +1,13 @@
+package com.mindtree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCheck {
+	  public static void main(String[] args) 
+	    {
+	        SpringApplication.run(DemoCheck.class, args);
+	    }
+
+}
---
+++ src/main/java/com/mindtree/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.mindtree.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	@RequestMapping("/no")
+	public String home() {
+		return "Spring boot is workingkkk!";
+	}
+}
