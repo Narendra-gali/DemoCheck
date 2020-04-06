c6f4218f5a9a9993118c7630621ee5bb9b3bce7c
Narendra-gali <62335917+Narendra-gali@users.noreply.github.com>
2020-04-06 08:52:06 +0000

    Update DemoController.java
    final

--- src/main/java/com/mindtree/controller/DemoController.java
+++ src/main/java/com/mindtree/controller/DemoController.java
@@ -8,6 +8,6 @@ public class DemoController {
 
 	@RequestMapping("/no")
 	public String home() {
-		return "Spring boot is workingkkk doneee  for testing on more time";
+		return "Spring boot is workingkkk doneee  for";
 	}
 }
